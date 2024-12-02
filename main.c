951e0e6be17693eb7db9b8453c0ad4446438d1da
Omar El Madouni <omar0madouni@gmail.com>
2024-12-02 17:47:42 +0100

    7

--- main.c
+++ main.c
@@ -4,26 +4,26 @@ int main()
 {
     char *str;
 int fd = open("brainrot.txt", O_RDWR);
+str = get_next_line(fd);
+printf("result = %s\n", str);
+free(str);
+str = get_next_line(fd);
+printf("result = %s\n", str);
+free(str);
+str = get_next_line(fd);
+printf("result = %s\n", str);
+free(str);
+str = get_next_line(fd);
+printf("result = %s\n", str);
+free(str);
+str = get_next_line(fd);
+printf("result = %s\n", str);
+free(str);
+str = get_next_line(fd);
+printf("result = %s\n", str);
+free(str);
 // int a = read(fd, NULL, 4);
 // printf("%d\n", a);
-str = get_next_line(fd);
-printf("result = %s\n", str);
-free(str);
-str = get_next_line(fd);
-printf("result = %s\n", str);
-free(str);
-str = get_next_line(fd);
-printf("result = %s\n", str);
-free(str);
-str = get_next_line(fd);
-printf("result = %s\n", str);
-free(str);
-str = get_next_line(fd);
-printf("result = %s\n", str);
-free(str);
-str = get_next_line(fd);
-printf("result = %s\n", str);
-free(str);
 close(fd);
 }
 
