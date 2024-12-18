6963e2cd7d92cee10bf42e91acf6784df600b592
nu-fd2 <omar0madouni@gmail.com>
2024-12-18 08:14:34 +0100

    header mzyan

--- get_next_line.h
+++ get_next_line.h
@@ -6,32 +6,31 @@
 /*   By: oel-mado <oel-mado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 17:24:43 by oel-mado          #+#    #+#             */
-/*   Updated: 2024/12/14 06:47:01 by oel-mado         ###   ########.fr       */
+/*   Updated: 2024/12/18 08:13:49 by oel-mado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-#include <limits.h>
-#include <stdio.h>
-#include <fcntl.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 5
-#endif
+char	*get_next_line(int fd);
 
-char    *get_next_line(int fd);
+char	*rdd(char *buff, int fd);
+char	*one_line(const char *buff);
+char	*updtt(char *buff);
 
+char	*ft_strdup(const char *s1);
+char	*ft_strchr(const char *str, int c);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlcpy(char *dst, const char *src, size_t n);
 size_t	ft_strlen(const char *who);
-size_t	ft_strlcat(char *dst, const char *src, size_t n);
-char    *ft_strdup(const char *s1);
-char	*ft_strchr(const char *str, int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t n);
 
 #endif //GET_NEXT_LINE_H
