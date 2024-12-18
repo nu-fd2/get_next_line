330a87dceeafec9518c94d4efb5d0f7097dd4813
nu-fd2 <omar0madouni@gmail.com>
2024-12-18 08:36:15 +0100

    12 l null check dzmr

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,17 @@
 /*   By: oel-mado <oel-mado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 11:04:15 by oel-mado          #+#    #+#             */
-/*   Updated: 2024/12/14 06:53:22 by oel-mado         ###   ########.fr       */
+/*   Updated: 2024/12/18 08:34:57 by oel-mado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char *rdd(char *buff, int fd)
+char	*rdd(char *buff, int fd)
 {
-	int i;
-	char *yup;
+	int		i;
+	char	*yup;
+	char	*wait;
 
 	yup = ft_calloc(BUFFER_SIZE + 1, 1);
 	if (!yup)
@@ -24,27 +25,22 @@ static char *rdd(char *buff, int fd)
 	while (!ft_strchr(yup, '\n') && i != 0)
 	{
 		i = read(fd, yup, BUFFER_SIZE);
-		if (i < 0)
-		{
-			free(yup);
-			return (NULL);
-		}
+		if (i == -1)
+			return (free(yup), yup = NULL, NULL);
 		yup[i] = '\0';
+		wait = buff;
 		buff = ft_strjoin(buff, yup);
 		if (!buff)
-		{
-			free(yup);
-			return (NULL);
-		}
+			return (free(yup), yup = NULL, free(wait), NULL);
+		free(wait);
 	}
-	free(yup);
-	return (buff);
+	return (free(yup), yup = NULL, buff);
 }
 
-static char *one_line(const char *buff)
+char	*one_line(const char *buff)
 {
-	size_t i;
-	char *lain;
+	size_t	i;
+	char	*lain;
 
 	lain = NULL;
 	i = 0;
@@ -52,57 +48,69 @@ static char *one_line(const char *buff)
 		return (NULL);
 	while (buff[i] && buff[i] != '\n')
 		i++;
-	lain = ft_calloc(i + 2, 1);
+	if (buff[i] == '\0')
+		i += 1;
+	else
+		i += 2;
+	lain = ft_calloc(i, 1);
 	if (!lain)
 		return (NULL);
-	ft_strlcpy(lain, buff, i + 2);
+	ft_strlcpy(lain, buff, i);
 	return (lain);
 }
 
-static char *updtt(char *buff)
+char	*updtt(char *buff)
 {
-	size_t i;
-	size_t j;
-	char *neo;
+	size_t	i;
+	size_t	j;
+	char	*neo;
 
 	i = 0;
 	while (buff[i] && buff[i] != '\n')
 		i++;
 	if (buff[i] == '\0')
-	{
-		free(buff);
-		return (NULL);
-	}
+		return (free(buff), buff = NULL, NULL);
 	j = ft_strlen(buff + i + 1);
+	if (j <= 0)
+		return (free(buff), buff = NULL, NULL);
 	neo = ft_calloc(j + 1, 1);
 	if (!neo)
-	{
-		free(buff);
-		return (NULL);
-	}
+		return (free(buff), buff = NULL, NULL);
 	ft_strlcpy(neo, buff + i + 1, j + 1);
-	free(buff);
-	return (neo);
+	return (free(buff), buff = NULL, neo);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	static char *buff;
-	char *lain;
+	static char	*buff;
+	char		*lain;
 
 	lain = NULL;
 	if (BUFFER_SIZE <= 0 || fd < 0 || read(fd, buff, 0) == -1)
-		return(NULL);
+	{
+		if (buff)
+			return (free(buff), buff = NULL, NULL);
+		return (NULL);
+	}
 	buff = rdd(buff, fd);
-	if(!buff)
+	if (!buff)
 		return (NULL);
 	lain = one_line(buff);
 	if (!lain)
-	{
-		free(buff);
-		buff = NULL;
-		return (NULL);
-	}
+		return (free(buff), buff = NULL, NULL);
 	buff = updtt(buff);
 	return (lain); 
+}
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	ln;
+	char	*s2;
+
+	ln = ft_strlen(s1) + 1;
+	s2 = malloc(sizeof(char) * ln);
+	if (!s2)
+		return (NULL);
+	ft_strlcpy(s2, s1, ln);
+	return (s2);
 }
